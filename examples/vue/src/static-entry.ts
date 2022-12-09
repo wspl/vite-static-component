ca57e4d97720ba5293be19ee11ad8911d5169046
wspl <widesense.plqws@gmail.com>
2022-12-09 22:46:43 +0800

    fix: example

--- examples/vue/src/static-entry.ts
+++ examples/vue/src/static-entry.ts
@@ -1,8 +1,13 @@
-import { createSSRApp } from 'vue'
+import { createSSRApp, createApp } from 'vue'
 import Static from './Static.vue'
 
 export function createStaticApp() {
-  return createSSRApp(Static)
+  // Currently, static generation is only used in production in order to reduce complexity in development.
+  if (import.meta.env.PROD) {
+    return createSSRApp(Static)
+  } else {
+    return createApp(Static)
+  }
 }
 
 export function renderStatic() {
