4041637f52bb97204b5b16e99f73dabbabfa7580
wspl <widesense.plqws@gmail.com>
2022-12-09 23:03:15 +0800

    feat: support windows

--- packages/vite-static-component/src/index.ts
+++ packages/vite-static-component/src/index.ts
@@ -5,24 +5,26 @@ import type { Plugin } from 'vite'
 import { parse } from 'node-html-parser'
 
 async function generateStatic(entry: string) {
+  const tempDir = path.resolve('.vite-static-component-temp')
+  const entryRelative = path.relative(tempDir, path.resolve(entry)).replaceAll('\\', '/')
   const generateJs = `
     import { renderToString } from 'vue/server-renderer'
-    import { createStaticApp } from '${path.resolve(entry)}'
+    import { createStaticApp } from '${entryRelative}'
     const app = createStaticApp()
     export function render() {
       return renderToString(app)
     }
   `
-  await fs.promises.mkdir('.vite-static-component-temp/out', { recursive: true })
-  await fs.promises.writeFile('.vite-static-component-temp/generate.js', generateJs)
+  await fs.promises.mkdir(path.join(tempDir, 'out'), { recursive: true })
+  await fs.promises.writeFile(path.join(tempDir, 'generate.js'), generateJs)
   execSync('vite build --ssr .vite-static-component-temp/generate.js --outDir .vite-static-component-temp/out')
 
-  const files = await fs.promises.readdir('.vite-static-component-temp/out')
-  const generateDistJs = path.resolve(`.vite-static-component-temp/out/${files.find(f => f.endsWith('.js'))}`)
+  const files = await fs.promises.readdir(path.join(tempDir, 'out'))
+  const generateDistJs = path.join(tempDir, 'out', files.find(f => f.endsWith('.js')) ?? '')
   await fs.promises.rename(generateDistJs, `${generateDistJs}.mjs`)
-  const { render } = await import(`${generateDistJs}.mjs`)
+  const { render } = await import(`file://${generateDistJs}.mjs`)
   const html = await render()
-  await fs.promises.rm('.vite-static-component-temp', { recursive: true, force: true })
+  await fs.promises.rm(tempDir, { recursive: true, force: true })
   return html
 }
 
